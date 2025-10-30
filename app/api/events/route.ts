17c50b99608985b498aac072eba9cc64f6fee830
Erdem Alptuğ Öksüz <erdemalpoksuz@gmail.com>
2025-10-30 10:07:29 +0300

    feature devr-5: Route created.

---
+++ app/api/events/route.ts
@@ -0,0 +1,17 @@
+import { NextRequest, NextResponse } from "next/server";
+import connectDB from "@/lib/mongodb";
+
+export async function POST(req: NextRequest) {
+  try {
+    await connectDB();
+
+    const formData = await req.formData();
+    let event;
+  } catch (e) {
+    console.error(e);
+    return NextResponse.json({
+      message: "Event Creation Failed",
+      error: e instanceof Error ? e.message : "Unknown",
+    });
+  }
+}
